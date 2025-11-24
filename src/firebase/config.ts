9b8339f6b106a66603afc800597f80ddc172b768
Luis Bree <LuisBree@gmail.com>
2025-11-24 20:22:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "task-ranker-hl2yj",
+  "appId": "1:729350653160:web:916fec7d6f99726980755d",
+  "apiKey": "AIzaSyABA1zy1ajRdMJSbf1m6kY2GruAQINfu7M",
+  "authDomain": "task-ranker-hl2yj.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "729350653160"
+};
